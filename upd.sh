9fbd3546787fe7b384127dd1133e2db2317a4c68
Reini Urban <rurban@cpanel.net>
2012-05-21 17:49:49 -0500

    upd.sh -a

--- upd.sh
+++ upd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 perl Cannes2012.txt |tee Cannes2012.sorted.html
 f="Cannes2012.txt Cannes2012.sorted.html"
-git add $f; git commit -m'upd'
+git commit -a -m'upd'
 scp -q -P 3000 $f rurban.xarch.at:film/
