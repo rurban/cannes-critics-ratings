217f39d0168b3bf6e6baa386400ae9e3a49a2285
Reini Urban <rurban@cpanel.net>
2012-05-22 13:46:35 -0500

    forgot git push

--- upd.sh
+++ upd.sh
@@ -3,5 +3,5 @@ cmt=upd
 [ -n $1 ] && cmt=$*
 perl Cannes2012.txt |tee Cannes2012.sorted.html
 f="Cannes2012.txt Cannes2012.sorted.html"
-git commit -a -m"$cmt"
+git commit -a -m"$cmt" && git push
 scp -q -P 3000 $f rurban.xarch.at:film/
