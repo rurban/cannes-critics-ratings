f26d327b9a82a22d05293f6d8199842d3e396a62
Reini Urban <rurban@cpanel.net>
2012-05-22 16:05:41 -0500

    Ruta Buciunaite 6x, stddev:3.28

--- upd.sh
+++ upd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cmt=upd
-[ -n $1 ] && cmt=$*
+[ -n "$1" ] && cmt="$*"
 perl Cannes2012.txt |tee Cannes2012.sorted.html
 f="Cannes2012.txt Cannes2012.sorted.html"
 git commit -a -m"$cmt" && git push
