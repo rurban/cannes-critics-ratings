58e0c3c12e009a0130aa901dedc1a284c8214baf
Reini Urban <rurban@cpanel.net>
2012-05-22 12:32:58 -0500

    IonCinema day6

--- upd.sh
+++ upd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cmt=upd
+[ -n $1 ] && cmt=$*
 perl Cannes2012.txt |tee Cannes2012.sorted.html
 f="Cannes2012.txt Cannes2012.sorted.html"
-git commit -a -m'upd'
+git commit -a -m"$cmt"
 scp -q -P 3000 $f rurban.xarch.at:film/
