b9a7350ff82b8033ab62a42e4ee956acaeaddfc4
Reini Urban <rurban@cpanel.net>
2012-05-24 16:31:00 -0500

    support private csvdownload.py from http://stackoverflow.com/questions/3287651/download-a-spreadsheet-from-google-docs

--- csv.sh
+++ csv.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 c="Cannes 2012 - Películas%2FFilms.csv"
-diff -bu ~/Downloads/"$c" "$c" || (mv ~/Downloads/"$c" "$c"; ./csvsplit.pl > xx)
+# http://stackoverflow.com/questions/3287651/download-a-spreadsheet-from-google-docs-using-python
+if [ -f csvdownload.py ]; then
+  ./csvdownload.py > ~/Downloads/"$c"
+fi
+diff -bu "$c" ~/Downloads/"$c" || (mv ~/Downloads/"$c" "$c"; ./csvsplit.pl > xx)
