a0628f90c2d17bcd1faec3839e7d31ae5ee3d7d4
Reini Urban <rurban@cpanel.net>
2012-05-24 22:23:03 -0500

    end of day9, in the fog, ioncinema

--- csv.sh
+++ csv.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 c="Cannes 2012 - Películas%2FFilms.csv"
 # http://stackoverflow.com/questions/3287651/download-a-spreadsheet-from-google-docs-using-python
-if [ -f csvdownload.py ]; then
-  #./csvdownload.py > ~/Downloads/"$c"
-fi
+# if [ -f csvdownload.py ]; then ./csvdownload.py > ~/Downloads/"$c"; fi
 diff -bu "$c" ~/Downloads/"$c" || (mv ~/Downloads/"$c" "$c"; ./csvsplit.pl > xx)
