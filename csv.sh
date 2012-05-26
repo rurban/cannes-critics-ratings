39f18cfd70c628a84a2fe1fe3fee8d2b34a8a4c0
Reini Urban <rurban@cpanel.net>
2012-05-26 10:21:09 -0500

    middle of the last day

--- csv.sh
+++ csv.sh
@@ -2,4 +2,5 @@
 c="Cannes 2012 - Películas%2FFilms.csv"
 # http://stackoverflow.com/questions/3287651/download-a-spreadsheet-from-google-docs-using-python
 # if [ -f csvdownload.py ]; then ./csvdownload.py > ~/Downloads/"$c"; fi
-diff -bu "$c" ~/Downloads/"$c" || (mv ~/Downloads/"$c" "$c"; ./csvsplit.pl > xx)
+diff -bu "$c" ~/Downloads/"$c" || \
+  (mv "$c" "$c.bak"; mv ~/Downloads/"$c" "$c"; ./csvsplit.pl > xx)
